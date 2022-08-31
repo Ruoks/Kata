9b5ecf6a3a76ad2bd0d93fc322b71d6d24125522
Rokas K <rookaaz@gmail.com>
2022-08-31 21:11:31 +0100

    added failed test for discount calculation

--- Kata.Tests/CheckoutTests.cs
+++ Kata.Tests/CheckoutTests.cs
@@ -109,4 +109,38 @@ public class CheckoutTests
         Assert.Equal(expected, price);
 
     }
+
+    [Theory]
+    [InlineData(new string[] { "A99", "A99", "A99" }, new double[] { 0.5, 0.5, 0.5 }, 1.3)]
+    [InlineData(new string[] { "B15", "B15", }, new double[] { 0.3, 0.3 }, 0.45)]
+
+    public void Total_With_Discount_Applied(string[] skus, double[] prices, decimal expected)
+    {
+        IEnumerable<IItem> products = new[]
+            {
+                new Item{SKU = "A99", Price = 0.50m},
+                new Item{SKU = "B15", Price = 0.30m},
+                new Item{SKU = "C40", Price = 0.60m},
+            };
+
+        IEnumerable<ISpecialOffer> discounts = new[]
+            {
+                new SpecialOffer{SKU = "A99", Quantity = 3, Value = 1.30m},
+                new SpecialOffer{SKU = "B15", Quantity = 2, Value = 0.45m}
+            };
+
+        var checkout = new Checkout(products, discounts);
+
+        for (int i = 0; i < skus.Length; i++)
+        {
+            Item item = new Item { SKU = skus[i], Price = (decimal)prices[i] };
+
+            checkout.Scan(item);
+
+        }
+        var price = checkout.Total();
+
+        Assert.Equal(expected, price);
+
+    }
 }
