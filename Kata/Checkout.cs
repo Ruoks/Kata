cbe2947ea5f491265e2f364370b17897cba144a8
Rokas K <rookaaz@gmail.com>
2022-08-31 20:26:20 +0100

    Add project files.

---
+++ Kata/Checkout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    namespace Kata
+    {
+        public class Checkout
+        {
+            public decimal Total()
+            {
+                return 0m;
+            }
+
+            public void Scan(Item item)
+            {
+
+            }
+        }
+
+        public class Item
+        {
+
+        }
+    }
+
+}
